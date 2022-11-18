e0bd186eddbb77fdafc2b16a11743a94956df2d9
Vadim <vadimtomin21@yandex.by>
2022-11-18 12:15:10 +0300

    add error and loader
    add error and loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { Suspense } from 'react';
 import './App.css';
+import ErrorBoundary from './auxiliaries/ErrorBoundary/ErrorBoundary';
+import Loader from './auxiliaries/Loader/Loader';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 
-function App() {
+function App() { 
+
   return (
-    <div className="wrapper">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <ErrorBoundary>
+      <Suspense fallback={<Loader />}>
+        <div className="wrapper"> 
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </Suspense>
+    </ErrorBoundary>   
   );
 }
 
