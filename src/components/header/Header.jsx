d3a7b04791d9d8c9ab4aa03d1191b031b4423c21
Vadim <vadimtomin21@yandex.by>
2022-11-18 11:11:49 +0300

    update header
    update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.module.css';
+import cl from './header.module.css';
 import logo from '../../images/logo.svg';
 import { useSelector } from 'react-redux';
 
@@ -8,14 +8,18 @@ const Header = () => {
   const title = 'ROCK, PAPER, SCISSORS';
   const score = useSelector(state => state.score.score);
   return (
-    <header>
-      <h1>{title}</h1>
-      <img src={logo} alt="logo" />
-      <section>
-        <h2>
+    <header className={cl.header}>
+      <h1 className={cl.h1}>{title}</h1>
+      <img 
+        className={cl.img}
+        src={logo} 
+        alt="logo" 
+      />
+      <section className={cl.section}>
+        <h2 className={cl.h2}>
           SCORE
         </h2>
-        <span>
+        <span className={cl.span}>
           {
             score
           }
